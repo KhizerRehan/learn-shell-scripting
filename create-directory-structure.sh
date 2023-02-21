007859454df1b2cf8bb1dabd14e6afd590424aed
khizerrehan <khizerrehan92@gmail.com>
2023-02-21 13:52:01 +0500

    Add basic scripts and resources

---
+++ create-directory-structure.sh
@@ -0,0 +1,19 @@
+# mkdir countries
+# cd countries
+# mkdir USA India UK
+# echo "Washington, D.C" > USA/capital.txt
+# echo "London" > UK/capital.txt
+# echo "New Delhi" > India/capital.txt
+# uptime
+
+mkdir countries
+cd countries
+
+mkdir USA India UK
+
+echo "Washington, D.C" > USA/capital.txt
+echo "London" > UK/capital.txt
+echo "New Delhi" > India/capital.txt
+
+mkdir countries
+cd countries
---
+++ resources/shell-scripting.md
@@ -0,0 +1,4 @@
+## Shell Scripting
+
+- [shell-scripting-for-beginners-course](https://github.com/kodekloudhub/shell-scripting-for-beginners-course)
+- [Shell-Scripting-kodekloud.pdf](https://kodekloud.com/wp-content/uploads/2022/01/Shell-Scripting-kodekloud.pdf)
